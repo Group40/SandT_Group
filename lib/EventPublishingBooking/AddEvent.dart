524531f06e1e42f5dd4cbb23a9219c2e415101af
piyumaldk <piyumaldk@gmail.com>
2020-05-30 23:54:30 +0530

    Validate the form and add a snack bar to verify

--- lib/EventPublishingBooking/AddEvent.dart
+++ lib/EventPublishingBooking/AddEvent.dart
@@ -14,6 +14,8 @@ class AddEvent extends StatefulWidget {
 }
 
 class AddEventState extends State<AddEvent> {
+  var _formKey = GlobalKey<FormState>();
+
   TextEditingController nameController = TextEditingController();
   TextEditingController dateController = TextEditingController();
   TextEditingController venueController = TextEditingController();
@@ -40,6 +42,24 @@ class AddEventState extends State<AddEvent> {
     });
   }
 
+  void showSnackBar(BuildContext context){
+    var snackBar = SnackBar(
+      backgroundColor: Colors.black54,
+      content:  Text (
+        'Are you sure?',
+        style: TextStyle(fontSize: 20, color: Colors.white70),
+      ),
+      action: SnackBarAction(
+        textColor: Colors.cyan,
+          label: "YES",
+          onPressed: () {
+            send();
+          }
+      ),
+    );
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
     TextStyle textStyle = Theme.of(context).textTheme.title;
@@ -47,7 +67,6 @@ class AddEventState extends State<AddEvent> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add a Event'),
-
         //Optional back button
         leading: IconButton(
             icon: Icon(Icons.arrow_back),
@@ -56,178 +75,186 @@ class AddEventState extends State<AddEvent> {
             }),
         //Optional back button ends
       ),
-      body: Form(
-        child: Padding(
-          padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
-          child: ListView(
-            children: <Widget>[
-              //First Element
-              Padding(
-                padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-                child: TextFormField(
-                  controller: nameController,
-                  validator: (String value){
-                    if(value.isEmpty){
-                      return 'Please enter term';
-                    }
-                    return null;
-                  },
-                  style: textStyle,
-                  onChanged: (value) {
-                    debugPrint('Something changed in Text Field');
-                  },
-                  decoration: InputDecoration(
-                      labelText: 'Title',
-                      labelStyle: textStyle,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0),
-                      )),
-                ),
-              ),
-
-              //Second Element
-              Padding(
-                padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-                child: TextFormField(
-                  controller: dateController,
-                  validator: (String value){
-                    if(value.isEmpty){
-                      return 'Please enter term';
-                    }
-                    return null;
-                  },
-                  style: textStyle,
-                  onChanged: (value) {
-                    debugPrint('Something changed in Text Field');
-                  },
-                  decoration: InputDecoration(
-                      labelText: 'Date',
-                      labelStyle: textStyle,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0),
-                      )),
-                ),
-              ),
-
-              //Third Element
-              Padding(
-                padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-                child: TextFormField(
-                  controller: venueController,
-                  validator: (String value){
-                    if(value.isEmpty){
-                      return 'Please enter term';
-                    }
-                    return null;
-                  },
-                  style: textStyle,
-                  onChanged: (value) {
-                    debugPrint('Something changed in Text Field');
-                  },
-                  decoration: InputDecoration(
-                      labelText: 'Venue',
-                      labelStyle: textStyle,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0),
-                      )),
-                ),
-              ),
-
-              //Fourth Element
-              Padding(
-                padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-                child: TextFormField(
-                  controller: descriptionController,
-                  validator: (String value){
-                    if(value.isEmpty){
-                      return 'Please enter term';
-                    }
-                    return null;
-                  },
-                  style: textStyle,
-                  onChanged: (value) {
-                    debugPrint('Something changed in Text Field');
-                  },
-                  decoration: InputDecoration(
-                      labelText: 'Description',
-                      labelStyle: textStyle,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0),
-                      )),
-                ),
-              ),
-
-              //Fifth Element
-              Padding(
-                padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-                child: TextFormField(
-                  controller: headCountController,
-                  validator: (String value){
-                    if(value.isEmpty){
-                      return 'Please enter term';
-                    }
-                    return null;
-                  },
-                  style: textStyle,
-                  onChanged: (value) {
-                    debugPrint('Something changed in Text Field');
-                  },
-                  decoration: InputDecoration(
-                      labelText: 'Head Count',
-                      labelStyle: textStyle,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0),
-                      )),
-                ),
-              ),
-
-              //Sixth Element
-              Padding(
-                padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-                child: Row(
+      body: Builder(
+        builder: (context) =>
+            Form(
+              key: _formKey,
+              child: Padding(
+                padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+                child: ListView(
                   children: <Widget>[
-                    Expanded(
-                      child: RaisedButton(
-                        color: Theme.of(context).primaryColorDark,
-                        textColor: Theme.of(context).primaryColorLight,
-                        child: Text(
-                          'Reset',
-                          textScaleFactor: 1.5,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            _reset();
-                          });
+                    //First Element
+                    Padding(
+                      padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                      child: TextFormField(
+                        controller: nameController,
+                        validator: (String value) {
+                          if (value.isEmpty) {
+                            return 'Please enter a Title';
+                          }
+                          return null;
                         },
+                        style: textStyle,
+                        onChanged: (value) {
+                          debugPrint('Something changed in Text Field');
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Title',
+                            labelStyle: textStyle,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                            )),
                       ),
                     ),
-                    Container(
-                      width: 5.0,
-                    ),
-                    Expanded(
-                      child: RaisedButton(
-                        color: Theme.of(context).primaryColorDark,
-                        textColor: Theme.of(context).primaryColorLight,
-                        child: Text(
-                          'Add',
-                          textScaleFactor: 1.5,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            debugPrint('Add button clicked');
-                            send();
-                          });
+
+                    //Second Element
+                    Padding(
+                      padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                      child: TextFormField(
+                        controller: dateController,
+                        validator: (String value) {
+                          if (value.isEmpty) {
+                            return 'Please enter the date';
+                          }
+                          return null;
                         },
+                        style: textStyle,
+                        onChanged: (value) {
+                          debugPrint('Something changed in Text Field');
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Date',
+                            labelStyle: textStyle,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                            )),
+                      ),
+                    ),
+
+                    //Third Element
+                    Padding(
+                      padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                      child: TextFormField(
+                        controller: venueController,
+                        validator: (String value) {
+                          if (value.isEmpty) {
+                            return 'Please enter the venue';
+                          }
+                          return null;
+                        },
+                        style: textStyle,
+                        onChanged: (value) {
+                          debugPrint('Something changed in Text Field');
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Venue',
+                            labelStyle: textStyle,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                            )),
+                      ),
+                    ),
+
+                    //Fourth Element
+                    Padding(
+                      padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                      child: TextFormField(
+                        controller: descriptionController,
+                        validator: (String value) {
+                          if (value.isEmpty) {
+                            return 'Please enter the description';
+                          }
+                          return null;
+                        },
+                        style: textStyle,
+                        onChanged: (value) {
+                          debugPrint('Something changed in Text Field');
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Description',
+                            labelStyle: textStyle,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                            )),
+                      ),
+                    ),
+
+                    //Fifth Element
+                    Padding(
+                      padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        controller: headCountController,
+                        validator: (String value) {
+                          if (value.isEmpty) {
+                            return 'Please enter the head count';
+                          }
+                          return null;
+                        },
+                        style: textStyle,
+                        onChanged: (value) {
+                          debugPrint('Something changed in Text Field');
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Head Count',
+                            labelStyle: textStyle,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                            )),
+                      ),
+                    ),
+
+                    //Sixth Element
+                    Padding(
+                      padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: RaisedButton(
+                              color: Theme.of(context).primaryColorDark,
+                              textColor: Theme.of(context).primaryColorLight,
+                              child: Text(
+                                'Reset',
+                                textScaleFactor: 1.5,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _reset();
+                                });
+                              },
+                            ),
+                          ),
+                          Container(
+                            width: 5.0,
+                          ),
+                          Expanded(
+                            child: RaisedButton(
+                              color: Theme.of(context).primaryColorDark,
+                              textColor: Theme.of(context).primaryColorLight,
+                              child: Text(
+                                'Add',
+                                textScaleFactor: 1.5,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  if (_formKey.currentState.validate()) {
+                                    showSnackBar(context);
+                                    debugPrint('Add button clicked');
+                                  }
+                                });
+                              },
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
+            ),
       ),
     );
+
     throw UnimplementedError();
   }
 
