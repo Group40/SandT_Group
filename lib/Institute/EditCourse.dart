519131ae8428eaf527c2e63ab4b7b1175c9cb95d
piyumaldk <piyumaldk@gmail.com>
2020-08-06 21:24:02 +0530

    Edit course

--- lib/Institute/EditCourse.dart
+++ lib/Institute/EditCourse.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
-// import 'EventPublishing.dart';
-// import 'RequestList.dart';
-// import 'ConfirmedList.dart';
 
-var url = "http://10.0.2.2:8080/updateEvent";
+import 'package:sandtgroup/Institute/AdminCourseList.dart';
+import './AdminCourseList.dart';
+
+var url2 = "http://10.0.2.2:8080/updateCourse";
 
 class EditCourse extends StatefulWidget {
   final String text;
@@ -19,51 +19,52 @@ class EditCourse extends StatefulWidget {
 
 class EditCourseState extends State<EditCourse> {
   EditCourseState(String text);
-  DateTime _dateTime = DateTime.now();
   var _formKey = GlobalKey<FormState>();
-  var event;
+  var course;
   String name = '';
   String id = '';
-  String date = '';
-  String venue = '';
+  String ageGroup = '';
+  String price = '';
+  String location = '';
   String description = '';
-  String headCount = '';
-  String available = '';
+  String url = '';
 
   Future<String> getData() async {
     http.Response response = await http.get(
-        Uri.encodeFull("http://10.0.2.2:8080/findAllEvents/" + widget.text),
+        Uri.encodeFull("http://10.0.2.2:8080/findAllCourses/" + widget.text),
         headers: {"Accept": "application/json"});
 
     this.setState(() {
-      event = jsonDecode(response.body);
-      id = event['id'];
-      name = event['name'];
-      date = event['date'];
-      venue = event['venue'];
-      description = event['description'];
-      headCount = event['headCount'];
-      available = event['available'];
+      course = jsonDecode(response.body);
+      id = course['id'];
+      name = course['name'];
+      ageGroup = course['ageGroup'];
+      price = course['price'];
+      location = course['location'];
+      description = course['description'];
+      url = course['url'];
     });
   }
 
   TextEditingController nameController = TextEditingController();
-  TextEditingController dateController = TextEditingController();
-  TextEditingController venueController = TextEditingController();
+  TextEditingController ageGroupController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
+  TextEditingController urlController = TextEditingController();
 
   void update() async {
     debugPrint('funtion called');
     var body = jsonEncode({
       'id': id,
       'name': nameController.text,
-      'date': dateController.text,
-      'venue': venueController.text,
+      'ageGroup': ageGroupController.text,
+      'price': priceController.text,
+      'location': locationController.text,
       'description': descriptionController.text,
-      'headCount': headCount,
-      'available': available
+      'url': urlController.text
     });
-    return await http.post(url, body: body, headers: {
+    return await http.post(url2, body: body, headers: {
       "Accept": "application/json",
       "content-type": "application/json"
     }).then((dynamic res) {
@@ -107,7 +108,7 @@ class EditCourseState extends State<EditCourse> {
               Navigator.pop(context, true);
               Navigator.pop(context, true);
               Navigator.push(context, MaterialPageRoute(builder: (context) {
-                // return EventPublishing();
+                return AdminCourse();
               }));
             }),
         //Optional back button ends
@@ -120,96 +121,6 @@ class EditCourseState extends State<EditCourse> {
                 EdgeInsets.only(top: 0.0, left: 10.0, right: 10.0, bottom: 5),
             child: ListView(
               children: <Widget>[
-                //Buttons
-                Padding(
-                  padding: EdgeInsets.only(top: 0, bottom: 15.0),
-                  child: Row(
-                    children: <Widget>[
-                      //Request Button
-                      Expanded(
-                        child: RaisedButton(
-                          color: Theme.of(context).primaryColor,
-                          textColor: Theme.of(context).accentColor,
-                          child: Text(
-                            'Request List',
-                            textScaleFactor: 1.5,
-                            style: TextStyle(
-                              color: Theme.of(context).accentColor,
-                            ),
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) {
-                                // return RequestList(text: id);
-                              }));
-                            });
-                          },
-                        ),
-                      ),
-
-                      //Confirm Button
-                      Expanded(
-                        child: RaisedButton(
-                          color: Theme.of(context).accentColor,
-                          textColor: Theme.of(context).primaryColorLight,
-                          child: Text(
-                            'Confirmed List',
-                            textScaleFactor: 1.5,
-                            style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                            ),
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) {
-                                // return ConfirmedList(text: id);
-                              }));
-                            });
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                //Capacity and Availability
-                Padding(
-                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-                  child: Row(
-                    children: <Widget>[
-                      //Capacity
-                      Expanded(
-                        child: Text(
-                          'Capacity : ' + headCount,
-                          textScaleFactor: 1.5,
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: 5.0,
-                      ),
-
-                      //Availability
-                      Expanded(
-                        child: Text(
-                          'Availabile : ' + available,
-                          textScaleFactor: 1.5,
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: 5.0,
-                      ),
-                    ],
-                  ),
-                ),
-
                 //Name Field
                 Padding(
                   padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
@@ -217,7 +128,7 @@ class EditCourseState extends State<EditCourse> {
                     controller: nameController..text = name,
                     validator: (String value) {
                       if (value.isEmpty) {
-                        return 'Please enter the Name';
+                        return 'Please enter a course name';
                       }
                       return null;
                     },
@@ -228,7 +139,7 @@ class EditCourseState extends State<EditCourse> {
                       debugPrint('Something changed in Text Field');
                     },
                     decoration: InputDecoration(
-                        labelText: 'Name',
+                        labelText: 'Course Name',
                         labelStyle: TextStyle(
                           color: Theme.of(context).primaryColor,
                         ),
@@ -244,91 +155,14 @@ class EditCourseState extends State<EditCourse> {
                   ),
                 ),
 
-                //Date Field
-                Padding(
-                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-                  child: Row(
-                    children: <Widget>[
-                      //Date Field
-                      Expanded(
-                        child: TextFormField(
-                          enabled: false,
-                          controller: dateController..text = date,
-                          validator: (String value) {
-                            if (value.isEmpty) {
-                              return 'Please enter the date';
-                            }
-                            return null;
-                          },
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                          ),
-                          onChanged: (value) {
-                            debugPrint('Something changed in Text Field');
-                          },
-                          decoration: InputDecoration(
-                              labelText: 'Date',
-                              labelStyle: TextStyle(
-                                color: Theme.of(context).primaryColor,
-                              ),
-                              disabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: Theme.of(context).primaryColor,
-                                    width: 2.0),
-                                borderRadius: BorderRadius.circular(35.0),
-                              ),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5.0),
-                              )),
-                        ),
-                      ),
-                      Container(
-                        width: 5.0,
-                      ),
-
-                      //Calendar Button
-                      Expanded(
-                        child: ButtonTheme(
-                          child: RaisedButton(
-                            color: Theme.of(context).accentColor,
-                            child: Text(
-                              'Take a date',
-                              style: TextStyle(
-                                color: Theme.of(context).primaryColor,
-                              ),
-                            ),
-                            onPressed: () {
-                              showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now(),
-                                firstDate: DateTime(2001),
-                                lastDate: DateTime(2222),
-                              ).then((inputDate) => {
-                                    setState(() {
-                                      if (inputDate != null) {
-                                        _dateTime = inputDate;
-                                        date = _dateTime
-                                            .toString()
-                                            .substring(0, 10);
-                                      }
-                                    }),
-                                  });
-                            },
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                //Venue Field
+                //Age Group Field
                 Padding(
                   padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
                   child: TextFormField(
-                    controller: venueController..text = venue,
+                    controller: ageGroupController..text = ageGroup,
                     validator: (String value) {
                       if (value.isEmpty) {
-                        return 'Please enter the venue';
+                        return 'Please enter age group';
                       }
                       return null;
                     },
@@ -339,7 +173,75 @@ class EditCourseState extends State<EditCourse> {
                       debugPrint('Something changed in Text Field');
                     },
                     decoration: InputDecoration(
-                        labelText: 'Venue',
+                        labelText: 'Age Group',
+                        labelStyle: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).primaryColor,
+                              width: 2.0),
+                          borderRadius: BorderRadius.circular(35.0),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        )),
+                  ),
+                ),
+
+                //Price Field
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: TextFormField(
+                    controller: priceController..text = price,
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Please enter the price';
+                      }
+                      return null;
+                    },
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    onChanged: (value) {
+                      debugPrint('Something changed in Text Field');
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'Price',
+                        labelStyle: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).primaryColor,
+                              width: 2.0),
+                          borderRadius: BorderRadius.circular(35.0),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        )),
+                  ),
+                ),
+
+                //Location Field
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: TextFormField(
+                    controller: locationController..text = location,
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Please enter the location';
+                      }
+                      return null;
+                    },
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    onChanged: (value) {
+                      debugPrint('Something changed in Text Field');
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'Location',
                         labelStyle: TextStyle(
                           color: Theme.of(context).primaryColor,
                         ),
@@ -375,6 +277,40 @@ class EditCourseState extends State<EditCourse> {
                     },
                     decoration: InputDecoration(
                         labelText: 'Description',
+                        labelStyle: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).primaryColor,
+                              width: 2.0),
+                          borderRadius: BorderRadius.circular(35.0),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        )),
+                  ),
+                ),
+
+                //Url Field
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: TextFormField(
+                    controller: urlController..text = url,
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Please enter the url for more';
+                      }
+                      return null;
+                    },
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    onChanged: (value) {
+                      debugPrint('Something changed in Text Field');
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'Url for more',
                         labelStyle: TextStyle(
                           color: Theme.of(context).primaryColor,
                         ),
