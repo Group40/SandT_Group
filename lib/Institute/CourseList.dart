18cafe86981b1a0960ebb6a7154e8aba0891667b
piyumaldk <piyumaldk@gmail.com>
2020-08-07 23:41:40 +0530

    Filters done

--- lib/Institute/CourseList.dart
+++ lib/Institute/CourseList.dart
@@ -13,6 +13,8 @@ class CourseList extends StatefulWidget {
 
 class CourseListState extends State<CourseList> {
   TextEditingController ageController = TextEditingController();
+  TextEditingController minPriceController = TextEditingController();
+  TextEditingController maxPriceController = TextEditingController();
 
   List data;
   var modal = false;
@@ -20,6 +22,8 @@ class CourseListState extends State<CourseList> {
   String url;
   String searchTerm;
   String age;
+  String minPrice;
+  String maxPrice;
 
   Future<String> getData() async {
     http.Response response = await http.get(
@@ -35,7 +39,6 @@ class CourseListState extends State<CourseList> {
           if (data[i]["name"]
               .toLowerCase()
               .contains(searchTerm.toLowerCase())) {
-            print(data[i]["name"]);
             data[j] = data[i];
             j++;
           }
@@ -51,7 +54,34 @@ class CourseListState extends State<CourseList> {
           int ageGroupMaxInt = int.parse(data[i]["ageGroupMax"]);
           int ageInt = int.parse(age);
           if ((ageGroupMinInt <= ageInt) && (ageInt <= ageGroupMaxInt)) {
-            print(data[i]["name"]);
+            data[j] = data[i];
+            j++;
+          }
+        }
+        data.length = j;
+      }
+    }
+    if (minPrice != null) {
+      if (minPrice.length != 0) {
+        var j = 0;
+        for (var i = 0; i < data.length; i++) {
+          int priceInt = int.parse(data[i]["price"]);
+          int minPriceInt = int.parse(minPrice);
+          if (minPriceInt <= priceInt) {
+            data[j] = data[i];
+            j++;
+          }
+        }
+        data.length = j;
+      }
+    }
+    if (maxPrice != null) {
+      if (maxPrice.length != 0) {
+        var j = 0;
+        for (var i = 0; i < data.length; i++) {
+          int priceInt = int.parse(data[i]["price"]);
+          int maxPriceInt = int.parse(maxPrice);
+          if (priceInt <= maxPriceInt) {
             data[j] = data[i];
             j++;
           }
@@ -218,7 +248,6 @@ class CourseListState extends State<CourseList> {
       itemBuilder: (BuildContext context, int position) {
         return Container(
           width: MediaQuery.of(context).size.width * 0.7,
-          height: 330,
           padding: EdgeInsets.symmetric(vertical: 10),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
@@ -227,15 +256,15 @@ class CourseListState extends State<CourseList> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
+              Divider(),
               Text(
-                "Filter Courses",
+                "Filter Courses by age",
                 style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
                     color: Colors.grey),
               ),
               Divider(),
-              Text("By Age"),
               Padding(
                 padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
                 child: TextFormField(
@@ -244,16 +273,6 @@ class CourseListState extends State<CourseList> {
                     WhitelistingTextInputFormatter.digitsOnly
                   ],
                   controller: ageController,
-                  validator: (String value) {
-                    if (value.isEmpty) {
-                      return 'Please enter an age group';
-                    }
-                    int valueInt = int.parse(value);
-                    if (1 > valueInt) {
-                      return 'Enter a real Age';
-                    }
-                    return null;
-                  },
                   style: TextStyle(
                     color: Theme.of(context).primaryColor,
                   ),
@@ -300,7 +319,137 @@ class CourseListState extends State<CourseList> {
                   },
                   color: Theme.of(context).primaryColor,
                 ),
-              )
+              ),
+              //Min Price
+              Divider(),
+              Text(
+                "Filter Courses by Price",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey),
+              ),
+              Divider(),
+              Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    WhitelistingTextInputFormatter.digitsOnly
+                  ],
+                  controller: minPriceController,
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  onChanged: (value) {
+                    debugPrint('Something changed in Text Field');
+                  },
+                  decoration: InputDecoration(
+                      labelText: 'Minimum price I can afford',
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.only(top: 0),
+                        // add padding to adjust icon
+                        child: Icon(
+                          Icons.view_agenda,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      labelStyle: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Theme.of(context).primaryColor, width: 2.0),
+                        borderRadius: BorderRadius.circular(0.0),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(0.0),
+                      )),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: RaisedButton(
+                  child: Text(
+                    "Set Filter",
+                    style: TextStyle(color: Theme.of(context).accentColor),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      minPrice = minPriceController.text;
+                      modal = !modal;
+                    });
+                    getData();
+                  },
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              //Max Price
+              Divider(),
+              Text(
+                "Filter Courses by Price",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey),
+              ),
+              Divider(),
+              Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    WhitelistingTextInputFormatter.digitsOnly
+                  ],
+                  controller: maxPriceController,
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  onChanged: (value) {
+                    debugPrint('Something changed in Text Field');
+                  },
+                  decoration: InputDecoration(
+                      labelText: 'Maximum price I can afford',
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.only(top: 0),
+                        // add padding to adjust icon
+                        child: Icon(
+                          Icons.view_agenda,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      labelStyle: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Theme.of(context).primaryColor, width: 2.0),
+                        borderRadius: BorderRadius.circular(0.0),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(0.0),
+                      )),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: RaisedButton(
+                  child: Text(
+                    "Set Filter",
+                    style: TextStyle(color: Theme.of(context).accentColor),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      maxPrice = maxPriceController.text;
+                      modal = !modal;
+                    });
+                    getData();
+                  },
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
             ],
           ),
         );
