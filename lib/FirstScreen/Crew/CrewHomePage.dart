5dc28aac836f2adf6d3f57e3eb263b6e39ad4b77
Sandun Weeraseakara <43146724+tharakasandun@users.noreply.github.com>
2020-08-23 14:24:43 +0530

    crew home page

--- lib/FirstScreen/Crew/CrewHomePage.dart
+++ lib/FirstScreen/Crew/CrewHomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sandtgroup/FirstScreen/Crew/CrewScreenGrids.dart';
 import 'CrewAppDrawer.dart';
 
 class CrewHomePage extends StatefulWidget {
@@ -12,20 +13,39 @@ class _CrewHomePageState extends State<CrewHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      backgroundColor: Colors.grey[300],
         key: _scaffoldKey,
         drawer: new CrewAppDrawer(),
         appBar: new AppBar(
           title: new Text(
-            "Crew Member",
+            "S & T Group",
             style: TextStyle(
                 color: Colors.black, fontSize: 22, fontWeight: FontWeight.bold),
           ),
         ),
-        body: Center(
-          child: Text(
-            "Crew Member Home Page ",
-            style: TextStyle(fontSize: 25),
-          ),
-        ));
+        body: Container(
+              //padding: EdgeInsets.all(20.0),
+              //height: 200,
+              child: Column(
+                mainAxisAlignment: (MainAxisAlignment.start),
+                //crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(height: 15.0),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Crew Panel",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  //SizedBox(height: 5.0),
+                  CrewScreenGrids()
+                ],
+              ),
+            ));
   }
 }
